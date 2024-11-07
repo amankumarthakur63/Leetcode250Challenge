9dc6baaabbe3a06e918609294256788d5ba0917a
amankumarthakur63 <amankumar005562@gmail.com>
2024-11-07 22:48:16 +0530

    commit this please

---
+++ src/Day4HIndex.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+
+public class Day4HIndex {
+
+    public int hIndex(int[] citations) {
+        int n = citations.length;
+        Arrays.sort (citations);
+
+        for (int i = 0; i < n; i++) {
+            if (citations[i] >=  n - i) {
+                return n - i;
+            }
+        }
+        return 0;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
