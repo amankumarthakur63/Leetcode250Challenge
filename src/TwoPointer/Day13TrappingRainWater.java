b11bd910f57b9f3abf9552ea6d4f2175a83f3194
amankumarthakur63 <amankumar005562@gmail.com>
2024-11-16 23:35:59 +0530

    commit this please

---
+++ src/TwoPointer/Day13TrappingRainWater.java
@@ -0,0 +1,30 @@
+package TwoPointer;
+
+public class Day13TrappingRainWater {
+
+    public int trap(int[] height) {
+        int left = 0;
+        int right = height.length - 1;
+        int leftMax = height[left];
+        int rightMax = height[right];
+        int water = 0;
+
+        while (left < right) {
+            if (leftMax < rightMax) {
+                left++;
+                leftMax = Math.max(leftMax, height[left]);
+                water += leftMax - height[left];
+            } else {
+                right--;
+                rightMax = Math.max(rightMax, height[right]);
+                water += rightMax - height[right];
+            }
+        }
+
+        return water;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
