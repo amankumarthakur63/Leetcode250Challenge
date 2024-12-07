4c4377e13c56416cc84e6dcec10089b4a9f3aea2
amankumarthakur63 <amankumar005562@gmail.com>
2024-12-07 22:02:38 +0530

    commit cnfig

---
+++ src/LinkedList/Day33ReverseLinkedList.java
@@ -0,0 +1,27 @@
+package LinkedList;
+
+public class Day33ReverseLinkedList {
+
+    public class ListNode {
+      int val;
+      ListNode next;
+      ListNode() {}
+      ListNode(int val) { this.val = val; }
+      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+
+    public ListNode reverseList(ListNode head) {
+        ListNode node = null;
+        while (head != null) {
+            ListNode temp = head.next;
+            head.next = node;
+            node = head;
+            head = temp;
+        }
+        return node;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
