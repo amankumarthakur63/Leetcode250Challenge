0d484d0b65595b18f65a0bf86748fd00d8990c64
amankumarthakur63 <amankumar005562@gmail.com>
2024-12-11 23:56:54 +0530

    commit config

---
+++ src/LinkedList/Day37RotateList.java
@@ -0,0 +1,42 @@
+package LinkedList;
+
+public class Day37RotateList {
+
+    public class ListNode {
+      int val;
+      ListNode next;
+      ListNode() {}
+      ListNode(int val) { this.val = val; }
+      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+
+    public ListNode rotateRight(ListNode head, int k) {
+        if (head == null || head.next == null || k == 0) {
+            return head;
+        }
+
+        int length = 1;
+        ListNode temp = head;
+
+        while (temp.next != null) {
+            temp = temp.next;
+            length++;
+        }
+
+        temp.next = head;
+        k = k % length;
+        k = length - k;
+
+        while (k-- > 0) {
+            temp = temp.next;
+        }
+
+        head = temp.next;
+        temp.next = null;
+
+        return head;
+    }
+    public static void main(String[] args) {
+
+    }
+}
