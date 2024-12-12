bc0778a4d8fa30ab0667939a8514e6f2c24a4719
amankumarthakur63 <amankumar005562@gmail.com>
2024-12-12 23:58:51 +0530

    commit config

---
+++ src/Tree/Day38SameTree.java
@@ -0,0 +1,31 @@
+package Tree;
+
+public class Day38SameTree {
+
+    public class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+    }
+
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) {
+            return true;
+        }
+        if (p != null &&  q != null && p.val == q.val) {
+            return (isSameTree (p.left, q.left) && isSameTree (p.right, q.right));
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
