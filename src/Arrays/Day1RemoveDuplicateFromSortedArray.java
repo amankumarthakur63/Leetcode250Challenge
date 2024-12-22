4b0928c2960cfb6a879c3deb7e5a85b6ac7e214f
amankumarthakur63 <amankumar005562@gmail.com>
2024-12-22 23:48:13 +0530

    commit config

--- src/Arrays/Day1RemoveDuplicateFromSortedArray.java
+++ src/Arrays/Day1RemoveDuplicateFromSortedArray.java
@@ -8,6 +8,9 @@ import java.util.Set;
 public class Day1RemoveDuplicateFromSortedArray {
 
     public int removeDuplicates(int[] nums) {
+        if (nums.length == 0) {
+            return 0;
+        }
         int j = 1;  // j is initialize by 1
         for (int i = 1; i < nums.length; i++) { // i is iterate from index 1 to array length
             if (nums[i] != nums[i - 1]) {       // if i is not equal to the previous index then
